e6e19fccb04c5c0843c4f9bf857ef31900c8dbf4
Sagar Darekar <sagardarekar1997@gmail.com>
2022-02-26 18:59:42 +0530

    Find the Kth smallest number in an array

--- data-structures/array/kth_element.js
+++ data-structures/array/kth_element.js
@@ -1,1 +1,27 @@
-// Find the Kth largest and Kth smallest number in an array
+// Find the Kth smallest number in an array
+function findKthSmall(arr, k) {
+    if (arr.length < k) {
+        return null;
+    }
+    sort(arr, k);
+    return arr[k - 1];
+}
+
+function sort(arr, k) {
+    for (let i = 0; i < k; i++) {
+        for (let j = i; j < arr.length; j++) {
+            //swap the elements
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+
+const arr = [1, 2, 3, 45, 5, 66, -21231];
+let k = 1;
+const kthSmall = findKthSmall(arr, k);
+console.log(k + "th smallest element is " + kthSmall);
