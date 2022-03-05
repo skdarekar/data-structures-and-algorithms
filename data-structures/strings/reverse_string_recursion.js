242b86dc3b9052e44ec4fd5ae6d607d03d66a9bb
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-05 22:08:14 +0530

    reverse string using recursion

--- data-structures/strings/reverse_string_recursion.js
+++ data-structures/strings/reverse_string_recursion.js
@@ -1,1 +1,11 @@
-//Wrtite a code to reverse a given string with recursion
+//Write a code to reverse a given string with recursion
+function reverse_string(str, len) {
+    if (len === 1) {
+        return str[0];
+    }
+    return str[len - 1] + reverse_string(str, len - 1);
+}
+
+let str = "abcdefghijklmnopqrstuvwxyz";
+const reversedString = reverse_string(str, str.length);
+console.log(reversedString);
