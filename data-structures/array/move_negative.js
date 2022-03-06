00400cb2d822dc238b567f943d57fde73a6df66d
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-06 16:57:46 +0530

    Move all negative elements to the end

--- data-structures/array/move_negative.js
+++ data-structures/array/move_negative.js
@@ -1,8 +1,23 @@
 // Move all negative elements to end
 function moveNegative(arr) {
     console.log(arr);
-    
+    let cnt = 0;
+    let temp;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            for (let j = 0; j < arr.length - 1; j++) {
+                cnt++;
+                if (arr[j] < 0) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    console.log(arr);
+    console.log("No of iterations:"+cnt);
 }
 
-let arr = [-1, 2, 34, -4, 5, 66, -55];
+let arr = [-1, -2, -1, -9];
 moveNegative(arr);
