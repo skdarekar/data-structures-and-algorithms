1029c2279e1829dc2a2a6888f166159974f0bd6c
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-07 10:56:42 +0530

    Shuffle array of 2n

--- data-structures/array/shuffle_array.js
+++ data-structures/array/shuffle_array.js
@@ -5,4 +5,14 @@
     Output: [2,3,5,4,1,7] 
     Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
 */
- 
+
+function suffleArray(arr, n) {
+    let newArr = [];
+    for (let i = 0; i < n; i++) {
+        newArr = [...newArr, arr[i], arr[n + i]]
+    }
+    console.log(newArr);
+}
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(arr);
+suffleArray(arr, Math.floor(arr.length / 2));
