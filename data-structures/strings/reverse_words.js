60cc7809d65d07954d08bd4f2817945c4082f612
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-09 10:35:37 +0530

    reverse words input statement updated

--- data-structures/strings/reverse_words.js
+++ data-structures/strings/reverse_words.js
@@ -1,1 +1,6 @@
-//Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+/* 
+    Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+    For example - 
+        Input - JS.is.Best 
+        Output - Best.is.JS
+*/
