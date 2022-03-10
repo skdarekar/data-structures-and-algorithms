1af668df935a35e68f39bda4fd6b082ec9c4bdb0
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-10 10:03:09 +0530

    reverse words

--- data-structures/strings/reverse_words.js
+++ data-structures/strings/reverse_words.js
@@ -4,3 +4,17 @@
         Input - JS.is.Best 
         Output - Best.is.JS
 */
+
+// using inbuild split method
+function reverseWords(words) {
+    let wordsSplit = words.split(".");
+    let reversedWords = [];
+    for (let i = wordsSplit.length - 1; i >= 0; i--) {
+        // console.log(wordsSplit[i]);
+        reversedWords.push(wordsSplit[i]);
+    }
+    console.log(reversedWords.join("."));
+}
+
+let words = "hello.world";
+reverseWords(words);
