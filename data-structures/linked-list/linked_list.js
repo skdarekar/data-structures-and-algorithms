337681a848c9ee207775a462160ad26d9f249a25
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-12 23:01:18 +0530

    linked list initial skeleton

---
+++ data-structures/linked-list/linked_list.js
@@ -0,0 +1,20 @@
+// Implement linked list
+class Node {
+    constructor(element){
+        this.element = element;
+        this.next = null;
+    }
+    // functions to be implemented
+    // addElement
+    // insertAt(element, location)
+    // removeFrom(location)
+    // removeElement(element)
+
+    // Helper methods
+    // isEmpty
+    // size_Of_list
+    // PrintList
+}
+
+let firstNode = new Node(10);
+console.log(firstNode.element)
