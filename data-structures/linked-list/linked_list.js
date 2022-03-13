e64bea1af1953163032b3c7952c93993b80a55d3
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-13 11:04:51 +0530

    linked list addElement and printList functions

--- data-structures/linked-list/linked_list.js
+++ data-structures/linked-list/linked_list.js
@@ -1,20 +1,57 @@
 // Implement linked list
 class Node {
-    constructor(element){
+    constructor(element) {
         this.element = element;
         this.next = null;
     }
+}
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
     // functions to be implemented
-    // addElement
+    // addElement(element)
+    addElement(element) {
+        let newNode = new Node(element);
+        let currentHead;
+        if (this.head) {
+            currentHead = this.head;
+            while (currentHead.next) {
+                currentHead = currentHead.next;
+            }
+            currentHead.next = newNode;
+        } else {
+            this.head = newNode;
+        }
+    }
+
     // insertAt(element, location)
     // removeFrom(location)
     // removeElement(element)
 
     // Helper methods
     // isEmpty
-    // size_Of_list
+
+    // sizeOfList
     // PrintList
+    printList() {
+        if (this.head) {
+            while (this.head) {
+                // console.log(` ${this.head.element} `);
+                process.stdout.write(` ${this.head.element} -> `);
+                this.head = this.head.next;
+            }
+        } else {
+            console.log("List is empty...!");
+        }
+    }
 }
 
-let firstNode = new Node(10);
-console.log(firstNode.element)
+let list = new LinkedList(null);
+list.addElement(10);
+list.addElement(12);
+list.addElement(13);
+list.addElement(15);
+list.addElement(17);
+list.addElement(19);
+list.printList();
