8785b5d311c8077dd25c079ae54a30eec49c2f25
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-18 09:27:52 +0530

    detect loop - linked list skeleton

--- data-structures/linked-list/detect_loop.js
+++ data-structures/linked-list/detect_loop.js
@@ -1,1 +1,27 @@
 // Detect loop in linkedList
+class Node {
+    constructor(element) {
+        this.element = element;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(head = null) {
+        this.head = head;
+    }
+
+    addElement(element) {
+        // todo
+    }
+
+    printList() {
+        // todo
+    }
+
+    detectLoop() {
+        // todo
+    }
+}
+
+let list = new LinkedList();
