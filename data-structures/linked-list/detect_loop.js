8e291b05f35bdf4233b68b7054f441bf2f82597a
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-18 09:49:45 +0530

    detect loop - added code to create loop in list

--- data-structures/linked-list/detect_loop.js
+++ data-structures/linked-list/detect_loop.js
@@ -33,6 +33,26 @@ class LinkedList {
                 process.stdout.write(`${tempHead.element} ${tempHead.next ? "=> " : ""}`);
                 tempHead = tempHead.next;
             }
+            console.log();
+        } else {
+            console.log("No data");
+        }
+    }
+
+    createLoop(atLocation) {
+        // create loop in linked list
+        if (this.head) {
+            let tempHead = this.head;
+            let loopHead = this.head;
+            let i = 1;
+            while (tempHead.next) {
+                if (i == atLocation) {
+                    loopHead = tempHead;
+                }
+                i++;
+                tempHead = tempHead.next;
+            }
+            tempHead.next = loopHead;
         } else {
             console.log("No data");
         }
@@ -48,3 +68,5 @@ list.addElement(10);
 list.addElement(11);
 list.addElement(12);
 list.printList();
+list.createLoop(1);
+// list.detectLoop();
