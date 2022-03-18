1075de4cc8c84e3e680faa381e5f8fc8355c3572
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-18 09:35:18 +0530

    linked list - detect loop - add and printlist functions added

--- data-structures/linked-list/detect_loop.js
+++ data-structures/linked-list/detect_loop.js
@@ -12,11 +12,30 @@ class LinkedList {
     }
 
     addElement(element) {
-        // todo
+        // add new element to end of linked list
+        let newNode = new Node(element);
+        if (this.head) {
+            let tempHead = this.head;
+            while (tempHead.next) {
+                tempHead = tempHead.next;
+            }
+            tempHead.next = newNode;
+        } else {
+            this.head = newNode;
+        }
     }
 
     printList() {
-        // todo
+        // print linked list elements
+        if (this.head) {
+            let tempHead = this.head;
+            while (tempHead) {
+                process.stdout.write(`${tempHead.element} ${tempHead.next ? "=> " : ""}`);
+                tempHead = tempHead.next;
+            }
+        } else {
+            console.log("No data");
+        }
     }
 
     detectLoop() {
@@ -25,3 +44,7 @@ class LinkedList {
 }
 
 let list = new LinkedList();
+list.addElement(10);
+list.addElement(11);
+list.addElement(12);
+list.printList();
