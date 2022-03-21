a34ffc7b01790553db8f18f707a3ba6b9aae4949
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-21 18:38:58 +0530

    string - count number of vowels and consonants in string

--- data-structures/strings/vowels_cons.js
+++ data-structures/strings/vowels_cons.js
@@ -1,2 +1,18 @@
 // Count number of vowels and consonants in string
-// Todo
+// Assuming given string contains only alphabets
+const vowels = "aeiou";
+function countVowelsCons(str) {
+    console.log("String:", str);
+    let vowelsCnt = 0, consCnt = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i])) {
+            vowelsCnt++;
+        } else {
+            consCnt++;
+        }
+    }
+    console.log("Number of vowels: ", vowelsCnt, " and consonants: ", consCnt);
+}
+
+const str = "abcdefg";
+countVowelsCons(str);
