5cb4c46a51792855821c50f2373bc8faa0068fe6
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-21 18:31:29 +0530

    string - Check if given string contain only digits

--- data-structures/strings/check_digits.js
+++ data-structures/strings/check_digits.js
@@ -1,2 +1,21 @@
 // Check if given string contain only digits
-// Todo
+function checkDigits(str) {
+    console.log("String:", str);
+    const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    let digitFlag = true;
+    for (let i = 0; i < str.length; i++) {
+        if (!digits.includes(parseInt(str[i]))) {
+            digitFlag = false;
+            break;
+        }
+    }
+    return digitFlag;
+}
+
+const str = "122345";
+const onlyDigits = checkDigits(str);
+if (onlyDigits) {
+    console.log("String contains only digits:", str);
+} else {
+    console.log("String does contains only digits:", str);
+}
