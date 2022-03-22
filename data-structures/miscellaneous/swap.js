a54522620c153aea65d24370fadd4bef25438bc5
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-22 16:26:27 +0530

    swap 2 numbers without 3rd variable

--- data-structures/miscellaneous/swap.js
+++ data-structures/miscellaneous/swap.js
@@ -1,2 +1,10 @@
 // Swap two numbers without using third variable
-// Todo
+function swap(a, b) {
+    console.log("a:", a, " b: ", b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    console.log("a:", a, " b: ", b);
+}
+
+swap(-120, 255);
