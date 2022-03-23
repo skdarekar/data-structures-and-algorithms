c205bd5e661f499c560a899c1479853cf144a88d
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-23 20:48:51 +0530

    anagram string example

--- data-structures/strings/anagrams.js
+++ data-structures/strings/anagrams.js
@@ -1,2 +1,5 @@
 // Check if two strings are anagrams of each other
+// An anagram of a string is another string that contains the same characters,
+// only the order of characters can be different. 
+// For example, “abcd” and “dabc” are an anagram of each other
 // Todo
