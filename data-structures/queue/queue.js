cab7e96476f773d4a91c8bdf556483d2eda5ac54
Sagar Darekar <sagardarekar1997@gmail.com>
2022-03-27 19:24:33 +0530

    Queue - queue implementation skeleton

--- data-structures/queue/queue.js
+++ data-structures/queue/queue.js
@@ -1,1 +1,13 @@
 // Queue implementation
+class Queue {
+    constructor(){
+        this.items = [];
+    }
+    // Todo
+    // functions to implement
+    // enqueue(item)
+    // dequeue()
+    // front()
+    // isEmpty()
+    // printQueue()
+}
