2c18a9fad62533574b0ef0ef480e15679abd8ea9
Sagar Darekar <41486724+skdarekar@users.noreply.github.com>
2022-04-15 09:42:10 +0530

    given number is prime number or not

--- data-structures/miscellaneous/prime_numbers.js
+++ data-structures/miscellaneous/prime_numbers.js
@@ -1,2 +1,19 @@
 // Find prime number upto given number
-// Todo
+function isPrime(number){
+        // console.log(number);
+        let i = 2, prime = true;
+        while(i <= Math.ceil(number/2)){
+                if(number % i === 0){
+                        prime = false;
+                        break;
+                }
+                i++;
+        }
+        if(prime)
+                console.log(number + " is prime number");
+        else
+                console.log(number + " is not prime number");
+}
+
+let num = 13;
+isPrime(num);
