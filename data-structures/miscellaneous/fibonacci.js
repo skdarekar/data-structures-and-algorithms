e3f2c96f70bc445c16f6615d8ab32a226cd77b52
Sagar Darekar <41486724+skdarekar@users.noreply.github.com>
2022-04-15 19:22:36 +0530

    Fibonacci series up to given number

--- data-structures/miscellaneous/fibonacci.js
+++ data-structures/miscellaneous/fibonacci.js
@@ -1,1 +1,11 @@
 // fibonacci series
+function printFibonacci(num){
+        let sum = 0;
+        for(let i = 0; i <= num; i++){
+                sum += i;
+                console.log("i:", i, sum);
+        }
+}
+
+let num = 5;
+printFibonacci(num);
